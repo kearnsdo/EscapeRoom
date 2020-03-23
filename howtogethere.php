b4703f56e0eaa5b4135bb4cfedfc56cc8197d68c
Donal Kearns <kearnsdo@tcd.ie>
2020-03-23 01:14:09 +0000

    Version 7 23 March Added Booking Calendar, images and video

--- howtogethere.php
+++ howtogethere.php
@@ -1,26 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<?php include 'includes/head.php'; ?>
-</head>
-<body>
-
-<?php include 'includes/navbar.php'; ?>
-
-<!--- Extra space for header -->
-<div class="row mt-5">
-  <div class="col-12 text-center mt-5">
-    <h3>How To Get Here</h3>
-  </div>
-</div>
+	<head>
+		<?php include 'includes/head.php'; ?>
+	</head>
+	<body>
+	<?php include 'includes/navbar.php'; ?>
+	<div class="container-fluid padding">
+		<div class="row welcome text-center">
+	<div class="col-12">
+		<h1 class="display-4">How To Get Here</h1>
+	</div>
 
 
+		<div class="row">
+			<div class="col-7">
+				    <div class="video-container">
+				    	<iframe width="100%" height="315px" src="HowToGet.mp4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				    </div>
+					</div>
+			<div class="col-4">
+					<div class="map-container">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.9699853459833!2d-6.256760284585479!3d53.34379347997847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e9b2dc78edd%3A0x2700fe0d8c716167!2sTrinity%20College%20Dublin!5e0!3m2!1sen!2sie!4v1583711944926!5m2!1sen!2sie" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+					</div>
+					</div>
 
+		<!--- Connect -->
+		<div class="container-fluid padding">
+			<div class="row text-center padding">
+				<div class="col-12">
+					<h2>Connect</h2>
+				</div>
+		<div class="col-12 social padding">
+			<a href="facebook.com/example/"><i class="fab fa-facebook"></i></a>
+					<a href="facebook.com/example/"><i class="fab fa-twitter"></i></a>
+									<a href="facebook.com/example/"><i class="fab fa-instagram"></i></a>
+											<a href="facebook.com/example/"><i class="fab fa-youtube"></i></a>
+			</div>
+		</div>
+		</div>
+		<!--- Footer -->
+		<?php include 'includes/footer.php'; ?>
 
-<hr class="my-4">  <!--- TCreates the long line -->
-</div>
-
-<?php include 'includes/footer.php' ?>
-
-</body>
+	</body>
 </html>
