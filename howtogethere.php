295ab2486b7d2703976765f06663c9afa6ae12ce
Donal Kearns <kearnsdo@tcd.ie>
2020-04-09 22:57:25 +0100

    Stopped video autoplay

--- howtogethere.php
+++ howtogethere.php
@@ -3,7 +3,7 @@
 	<head>
 	<title>Directions</title>
 		<?php include 'includes/head.php'; ?>
-		<meta name="description" content="Not sure how to get to Meabhrach Escape Rooms? Here you can find directions, a link to Google Maps and a video on how to get to our location from the nearest Luas stop">
+	<meta name="description" content="Not sure how to get to Meabhrach Escape Rooms? Here you can find directions to get to our location by bus or by Luas">
 		<meta name="keywords" content="public transport, bus, Luas, Meabhrach Escape Rooms, Escape Room, Dublin">
 		<meta name="author" content="Group 2">
 	</head>
