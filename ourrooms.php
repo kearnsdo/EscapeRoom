6349e0728439bdf38f91fa2437855040ee2aa119
Donal Kearns <kearnsdo@tcd.ie>
2020-04-09 17:44:47 +0100

    Darkened white room images
    and added hover over our rooms icons

--- ourrooms.php
+++ ourrooms.php
@@ -3,15 +3,10 @@
 <head>
     <title>Our Rooms</title>
 <?php include 'includes/head.php'; ?>
-  <meta name="description" content="An overview of the different escape rooms we offer. Click to see more. ">
-  <meta name="keywords" content="activity, fun, Meabhrach Escape Rooms, Escape Room, Dublin, escape room, Wicked Mirror, Mental Hospital, White Room, offer ">
-  <meta name="author" content="Group 2">
 </head>
 <body>
 
 <?php $page = 'ourrooms.php'; include 'includes/navbar.php'; ?>
-
-
 
 
 <!--- Welcome Section -->
@@ -29,17 +24,17 @@
 <div class="container-fluid padding">
 	<div class="row text-center padding">
 <div class="col-xs-12 col-sm-6 col-md-4">
-  <a href="mirror.php"><img src="img/icon_crazy.png" alt="mirror room icon, link"></a>
+  <a href="mirror.php">  <span id="edit-img" class="img-span pull-right"><img src="img/icon_crazy.png" alt="mirror room icon, link"></span></a>
 	<h3>Wicked Mirrors</h3>
 	<p>Discover what stands behind the cold reflective surface.</p>
 </div>
 <div class="col-xs-12 col-sm-6 col-md-4">
-  <a href="mental.php"><img src="img/icon_scary.png" alt="mental room icon, link"></a>
+  <a href="mental.php"> <span id="edit-img" class="img-span pull-right"><img src="img/icon_scary.png" alt="mental room icon, link"></span></a>
 	<h3>Mental Hospital</h3>
 	<p>Unveil the dark secrets of the mental hospital and get out alive.</p>
 </div>
 <div class="col-sm-12 col-md-4">
-  <a href="white.php"><img src="img/icon_actor.png" alt="white room icon, link"></a>
+  <a href="white.php"><span id="edit-img" class="img-span pull-right"><img src="img/icon_actor.png" alt="white room icon, link"></span></a>
 	<h3>White Room</h3>
 	<p>Enjoy your trip to the world of colours and nothingness.</p>
 </div>
@@ -47,11 +42,10 @@
 <hr class="my-4">
 </div>
 
-
-
-
-
-
+<style> #edit-img:hover img {
+     -webkit-filter: invert(100%);
+ }
+</style>
 
 </body>
 </html>
