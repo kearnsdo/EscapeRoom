d16b0fdbf6edb32110ec2fa78c46f76a938ead6d
Donal Kearns <kearnsdo@tcd.ie>
2020-04-10 11:33:47 +0100

    Muted escape room

--- howtogethere.php
+++ howtogethere.php
@@ -17,14 +17,14 @@
 	<div class="row">
 		<div class="col-7">
 					<div class="video-container">
-						<iframe width="100%" height="315px" src="HowToGet.mp4" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						<video width="375px" height="240px" src="HowToGet.mp4" allowfullscreen controls muted></video>
 					</div>
 				</div>
 		<div class="col-4">
 				<div class="map-container">
 					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.9699853459833!2d-6.256760284585479!3d53.34379347997847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e9b2dc78edd%3A0x2700fe0d8c716167!2sTrinity%20College%20Dublin!5e0!3m2!1sen!2sie!4v1583711944926!5m2!1sen!2sie" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 				</div>
-			
+
 					<a href="howtogetheretext.php">Click here for screen readers</a>
 
 
