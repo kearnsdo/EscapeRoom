8abea15898875e0fa2b307f91a23aacec34ffb17
kearnsdo <61514979+kearnsdo@users.noreply.github.com>
2020-04-10 16:03:51 +0100

    Added titles

--- book3.php
+++ book3.php
@@ -25,7 +25,7 @@ if(isset($_POST['submit'])){
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title></title>
+<title>White Room Calendar</title>
 
   <link rel="stylesheet" href="/css/main.css">
 </head>
